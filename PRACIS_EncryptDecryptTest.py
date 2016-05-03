e1086eba00d12badd21aac25414d1d0310081fa8
jmdefuentes <jfuentes@inf.uc3m.es>
2016-05-03 15:18:50 +0200

    Stable version with bit-level operations

--- PRACIS_EncryptDecryptTest.py
+++ PRACIS_EncryptDecryptTest.py
@@ -8,6 +8,7 @@
 # Copyright (c) 2015, The MITRE Corporation
 #All rights reserved.
 
+
 #Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are met:
 #    * Redistributions of source code must retain the above copyright
